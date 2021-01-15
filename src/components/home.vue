863bb7b9d13b3ce13e7944a414caebc62765e6b0
草莓奶昔 <8306432+caomeinaixi@user.noreply.gitee.com>
2021-01-15 19:31:36 +0800

    ok

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,58 +1,40 @@
 <template>
   <div id="home" class="rwl-exempt" @keydown.ctrl="test">
     <!-- 关于对话框 -->
-    <el-dialog
-      title="关于本站"
-      width="60%"
-      center
-      :visible.sync="centerDialogVisible"
-      @close="closeDialog"
-      custom-class="el-opacity"
-    >
+    <el-dialog title="关于本站" width="60%" center :visible.sync="centerDialogVisible" @close="closeDialog" custom-class="el-opacity">
       <about @shut="childShutDialog" @open="childOpenDrawer"></about>
     </el-dialog>
     <!-- 登陆的drawer -->
-    <el-drawer
-      direction="ltr"
-      :visible="drawerVisible"
-      custom-class="el-opacity"
-      @close="closeDrawer"
-      size="20%"
-    >
+    <el-drawer direction="ltr" :visible="drawerVisible" custom-class="el-opacity" @close="closeDrawer" size="20%">
       <login></login>
       <signUp v-if="isSignUp"></signUp>
     </el-drawer>
     <!-- 导航nav -->
     <el-header class="nav " ref="nav" :class="{ navShow: isnavShow }">
       <el-row>
-        <el-col :span="8"
-          ><a href="/" class="left"
-            ><div class="el-icon-milk-tea icon"></div>
+        <el-col :span="8"><a href="/" class="left">
+            <div class="el-icon-milk-tea icon"></div>
             草莓奶昔的语料库小站
           </a>
         </el-col>
-        <el-col :span="16"
-          ><a class="right" @click="drawerVisible = true"
-            ><div class="el-icon-user-solid icon"></div>
+        <el-col :span="16"><a class="right" @click="drawerVisible = true">
+            <div class="el-icon-user-solid icon"></div>
             我的
           </a>
-          <a class="middle" @click="moveTo(2, 1)"
-            ><div class="el-icon-star-on icon"></div>
+          <a class="middle" @click="moveTo(2, 1)">
+            <div class="el-icon-star-on icon"></div>
             收藏
           </a>
-          <a class="middle" @click="centerDialogVisible = true"
-            ><div class="el-icon-lollipop icon"></div>
+          <a class="middle" @click="centerDialogVisible = true">
+            <div class="el-icon-lollipop icon"></div>
             关于
           </a>
-          <a
-            href="https://space.bilibili.com/13169607/article"
-            class="middle"
-            target="_blank"
-            ><div class="el-icon-edit-outline icon"></div>
+          <a href="https://space.bilibili.com/13169607/article" class="middle" target="_blank">
+            <div class="el-icon-edit-outline icon"></div>
             文章
           </a>
-          <a class="middle" @click="moveTo(2, 0)"
-            ><div class="el-icon-folder-opened icon"></div>
+          <a class="middle" @click="moveTo(2, 0)">
+            <div class="el-icon-folder-opened icon"></div>
             搜索
           </a>
         </el-col>
@@ -62,13 +44,9 @@
     <!-- 整个应用开始 -->
     <full-page :options="options" ref="fullpage">
       <!-- 第一屏 -->
-      <div
-        class="section"
-        id="home1"
-        :style="{
+      <div class="section" id="home1" :style="{
           backgroundImage: 'url(' + random_arr[random_home_img_index] + ')'
-        }"
-      >
+        }">
         <!-- 中部 -->
         <div class="center_info">
           <h1 class="neon" @click="moveTo(2, 0)">CMNX_CORPUS</h1>
@@ -81,49 +59,28 @@
             <!-- 社交图片 -->
             <div class="social_info">
               <li id="bg-pre">
-                <img
-                  class="flipx"
-                  src="../assets/next-b.svg"
-                  @click="image_index_minus"
-                />
+                <img class="flipx" src="../assets/next-b.svg" @click="image_index_minus" />
               </li>
               <li class="bilibili">
-                <a
-                  href="https://space.bilibili.com/13169607"
-                  target="_blank"
-                  title="bilibili"
-                >
+                <a href="https://space.bilibili.com/13169607" target="_blank" title="bilibili">
                   <img src="../assets/bilibili.png" />
                 </a>
               </li>
               <li class="qq">
-                <a
-                  href="tencent://message/?uin=981377660&Site=JooIT.com&Menu=yes"
-                  title="qq"
-                >
+                <a href="tencent://message/?uin=981377660&Site=JooIT.com&Menu=yes" title="qq">
                   <img src="../assets/qq.png" />
                 </a>
               </li>
               <li class="wechat">
                 <a href="javaScript:void(0)" title="wechat">
-                  <el-popover
-                    placement="bottom"
-                    trigger="hover"
-                    close-delay="0"
-                  >
-                    <img
-                      src="../assets/myWechat.jpg"
-                      style="width:100px;height:100px"
-                    />
+                  <el-popover placement="bottom" trigger="hover" close-delay="0">
+                    <img src="../assets/myWechat.jpg" style="width:100px;height:100px" />
                     <img slot="reference" src="../assets/wechat.png" />
                   </el-popover>
                 </a>
               </li>
               <li class="wangyicloud" title="wangyicloud">
-                <a
-                  href="https://music.163.com/#/my/m/music/playlist?id=560596630"
-                  target="_blank"
-                >
+                <a href="https://music.163.com/#/my/m/music/playlist?id=560596630" target="_blank">
                   <img src="../assets/wangyiyun.png" />
                 </a>
               </li>
@@ -140,36 +97,21 @@
       </div>
       <!-- glitch特效 -->
       <div class="glitch set">
-        <div
-          class="glitch_img"
-          :style="{
+        <div class="glitch_img" :style="{
             backgroundImage: 'url(' + random_arr[random_home_img_index] + ')'
-          }"
-        ></div>
-        <div
-          class="glitch_img"
-          :style="{
+          }"></div>
+        <div class="glitch_img" :style="{
             backgroundImage: 'url(' + random_arr[random_home_img_index] + ')'
-          }"
-        ></div>
-        <div
-          class="glitch_img"
-          :style="{
+          }"></div>
+        <div class="glitch_img" :style="{
             backgroundImage: 'url(' + random_arr[random_home_img_index] + ')'
-          }"
-        ></div>
-        <div
-          class="glitch_img"
-          :style="{
+          }"></div>
+        <div class="glitch_img" :style="{
             backgroundImage: 'url(' + random_arr[random_home_img_index] + ')'
-          }"
-        ></div>
-        <div
-          class="glitch_img"
-          :style="{
+          }"></div>
+        <div class="glitch_img" :style="{
             backgroundImage: 'url(' + random_arr[random_home_img_index] + ')'
-          }"
-        ></div>
+          }"></div>
       </div>
       <!-- 第二屏 -->
       <div class="section" id="home2">
@@ -188,20 +130,20 @@
 
 <script>
 // 改变图片的方式：import图片存入数组，style绑定
-import steins0 from "../assets/steins0.jpg";
-import lovelive0 from "../assets/lovelive0.jpg";
-import arknights2 from "../assets/arknights2.jpg";
-import arknights3 from "../assets/arknights3.jpg";
-import arknights4 from "../assets/arknights4.jpg";
-import arknights7 from "../assets/arknights7.jpg";
-import arknights8 from "../assets/arknights8.jpg";
-import arknights9 from "../assets/arknights9.jpg";
+import steins0 from '../assets/steins0.jpg'
+import lovelive0 from '../assets/lovelive0.jpg'
+import arknights2 from '../assets/arknights2.jpg'
+import arknights3 from '../assets/arknights3.jpg'
+import arknights4 from '../assets/arknights4.jpg'
+import arknights7 from '../assets/arknights7.jpg'
+import arknights8 from '../assets/arknights8.jpg'
+import arknights9 from '../assets/arknights9.jpg'
 
-import Corpus from "./Corpus";
-import About from "./About";
-import Like from "./Like";
-import Login from "../components/beforeEnter/Login";
-import SignUp from "../components/beforeEnter/SignUp";
+import Corpus from './Corpus'
+import About from './About'
+import Like from './Like'
+import Login from '../components/beforeEnter/Login'
+import SignUp from '../components/beforeEnter/SignUp'
 
 export default {
   data() {
@@ -212,18 +154,18 @@ export default {
         navigation: false,
         keyboardScrolling: false,
         controlArrows: false,
-        normalScrollElements: ".el-drawer,.nav",
+        normalScrollElements: '.el-drawer,.nav',
         // 内容超过满屏时是否显示滚动条
         scrollOverflow: true,
         //Section滚动前的回调函数onLeave (index, nextIndex, direction),从0开始计算
         onLeave: (index, nextIndex, direction) => {
           // console.log(this);
           if (nextIndex.index == 0) {
-            this.currentSlide = this.$refs.fullpage.api.getActiveSlide().index;
-            this.isnavShow = false;
+            this.currentSlide = this.$refs.fullpage.api.getActiveSlide().index
+            this.isnavShow = false
           } else {
             if (this.currentSlide == 0) {
-              this.isnavShow = true;
+              this.isnavShow = true
             }
           }
         },
@@ -237,9 +179,9 @@ export default {
         ) => {
           // console.log(index.index, slideIndex.index, nextSlideIndex);
           if (slideIndex.index == 1) {
-            this.isnavShow = false;
+            this.isnavShow = false
           } else {
-            this.isnavShow = true;
+            this.isnavShow = true
           }
         }
       },
@@ -267,11 +209,11 @@ export default {
       drawerVisible: false,
       //是否显示注册组件
       isSignUp: false
-    };
+    }
   },
 
   created() {
-    this.set_bgimg_interval();
+    this.set_bgimg_interval()
   },
 
   mounted() {},
@@ -290,62 +232,62 @@ export default {
         this.random_home_img_index =
           this.random_home_img_index == this.random_arr.length - 1
             ? 0
-            : this.random_home_img_index + 1;
-      }, 10000);
+            : this.random_home_img_index + 1
+      }, 10000)
     },
     image_index_plus() {
       if (this.isclick1) {
-        this.isclick1 = false;
-        this.isclick2 = false;
+        this.isclick1 = false
+        this.isclick2 = false
         this.random_home_img_index =
           this.random_home_img_index == this.random_arr.length - 1
             ? 0
-            : this.random_home_img_index + 1;
-        clearInterval(this.bg_img_interval);
-        this.set_bgimg_interval();
+            : this.random_home_img_index + 1
+        clearInterval(this.bg_img_interval)
+        this.set_bgimg_interval()
         setTimeout(() => {
-          this.isclick1 = true;
-          this.isclick2 = true;
-        }, 1000);
+          this.isclick1 = true
+          this.isclick2 = true
+        }, 1000)
       }
     },
     image_index_minus() {
       if (this.isclick2) {
-        this.isclick2 = false;
-        this.isclick1 = false;
+        this.isclick2 = false
+        this.isclick1 = false
         this.random_home_img_index =
           this.random_home_img_index == 0
             ? this.random_arr.length - 1
-            : this.random_home_img_index - 1;
-        clearInterval(this.bg_img_interval);
-        this.set_bgimg_interval();
+            : this.random_home_img_index - 1
+        clearInterval(this.bg_img_interval)
+        this.set_bgimg_interval()
         setTimeout(() => {
-          this.isclick2 = true;
-          this.isclick1 = true;
-        }, 1000);
+          this.isclick2 = true
+          this.isclick1 = true
+        }, 1000)
       }
     },
     //滚动到第几页，第几个幻灯片；页面从1计算，幻灯片从0计算;
     moveTo(section, slide) {
-      this.$refs.fullpage.api.moveTo(section, slide);
+      this.$refs.fullpage.api.moveTo(section, slide)
     },
     closeDialog() {
-      this.centerDialogVisible = false;
+      this.centerDialogVisible = false
     },
     closeDrawer() {
-      this.drawerVisible = false;
+      this.drawerVisible = false
     },
     childShutDialog(value) {
-      this.centerDialogVisible = value;
+      this.centerDialogVisible = value
     },
     childOpenDrawer(value) {
-      this.drawerVisible = value;
+      this.drawerVisible = value
     }
   }
-};
+}
 </script>
 
 <style  lang='less' scoped >
-@import "../assets/css/glitch.css";
-@import "../assets/css/home1.css";
+@import '../assets/css/glitch.css';
+@import '../assets/css/home1.css';
 </style>
